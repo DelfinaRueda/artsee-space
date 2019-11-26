a9d6bb4d4b9a9f3fd566c8898e14bb6a1fc355ce
Julian Steinmann <steinmann.julian@gmail.com>
2019-11-26 16:53:35 +0000

    Give access rights to artist page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home artists]
+  skip_before_action :authenticate_user!, only: %i[home artists artist]
 
   def home
     @users = User.all
