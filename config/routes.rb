040fcce9fe7b86e834167ee811219892ea4aae14
Leon Lee <jtllee1@gmail.com>
2019-11-27 16:28:50 +0000

    added function to add own artwork and to display them

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/artists/', to: 'pages#artists'
   get '/artists/:id', to: 'pages#artist', as: :artist
-  resources :artworks, only: [:show, :index] do
+  resources :artworks do
     resources :comments, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
