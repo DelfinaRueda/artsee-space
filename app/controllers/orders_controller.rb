285cb6c7d9f8ab7b487d4136355d9294570c8702
Leon Lee <jtllee1@gmail.com>
2019-11-28 13:55:57 +0000

    added orders page and button to nav

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = current_user.orders.where(params[:id])
+  end
+
   def show
     @order = current_user.orders.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :artworks do
     resources :comments, only: [:create]
   end
-  resources :orders, only: [:show, :create] do
+  resources :orders, only: [:index, :show, :create] do
     resources :payments, only: :new
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
