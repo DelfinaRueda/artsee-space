9796875bc9b0795d69acb7388c3ac27f81f59028
Leon Lee <jtllee1@gmail.com>
2019-11-28 13:30:07 +0000

    made edit to currency factor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,14 +5,14 @@ class OrdersController < ApplicationController
 
   def create
     artwork = Artwork.find(params[:artwork_id])
-    order  = Order.create!(artwork: artwork, artwork_sku: artwork.title, amount: artwork.price_cents, state: 'pending', user: current_user)
+    order  = Order.create!(artwork: artwork, amount: artwork.price_cents, state: 'pending', user: current_user)
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
         name: artwork.title,
-        images: [artwork.photo.key],
-        amount: artwork.price_cents,
+        images: [url_for artwork.photo],
+        amount: (artwork.price_cents) * 100,
         currency: 'gbp',
         quantity: 1
       }],
