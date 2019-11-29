0a4c00bfb8a07e2be4be47f006a0f5c866a931f9
Julian Steinmann <steinmann.julian@gmail.com>
2019-11-29 14:12:24 +0000

    Move stripe route up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'pages#home'
   get '/artists/', to: 'pages#artists'
@@ -9,6 +10,5 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create] do
     resources :payments, only: :new
   end
-  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
