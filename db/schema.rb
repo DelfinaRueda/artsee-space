a23d9e1cc5901a75a01b6e376298ccfcfa8dd9b7
Delfina Rueda <delfinarueda@gmail.com>
2019-12-02 20:18:52 +0000

    Style to events page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_12_02_104908) do
-
+ActiveRecord::Schema.define(version: 2019_12_02_163436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +45,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_104908) do
     t.datetime "updated_at", null: false
     t.string "artwork_pic"
     t.string "title"
-    t.integer "price_cents", default: 0, null: false
     t.text "dimensions"
+    t.integer "price_cents", default: 0, null: false
     t.index ["gallery_id"], name: "index_artworks_on_gallery_id"
     t.index ["user_id"], name: "index_artworks_on_user_id"
   end
