f8854fc8135d5d6c15e33b8c4fe82dc8f127588e
Julian Steinmann <steinmann.julian@gmail.com>
2019-12-02 20:15:40 +0000

    Split up profile in seperate pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,8 @@ $(document).ready(function() {
   $('li.active').removeClass('active');
   $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 });
+
+$(document).ready(function() {
+  $('.artist-nav-item.active').removeClass('active');
+  $('a[href="' + location.pathname + '"]').closest('.artist-nav-item').addClass('active');
+});
