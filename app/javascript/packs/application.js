ea607594e16cfb0779480b4f04b294e43168e78c
Delfina Rueda <delfinarueda@gmail.com>
2019-12-02 17:50:17 +0000

    added events and map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+
+$(document).ready(function() {
+  $('li.active').removeClass('active');
+  $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
+});
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
