2587e44e8020538f2874d02d65905f49c2fe22ec
Julian Steinmann <steinmann.julian@gmail.com>
2019-12-05 15:33:37 +0000

    Test to fix redirect orders

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,5 +2,6 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
+    redirect_to orders
   end
 end
