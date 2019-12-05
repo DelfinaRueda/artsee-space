c2d1a3ac46a52bfbe5042b1bbeadfeafbc1f617a
Julian Steinmann <steinmann.julian@gmail.com>
2019-12-05 15:47:31 +0000

    undo test

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,6 +2,5 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
-    redirect_to orders
   end
 end
